77c292baa3c4220e96875ff7c401038e2de2706c
bigbugboy <liuxu-ynl@foxmail.com>
2023-05-22 13:55:34 +0800

    分割图片

--- puzzle_game/new_main.py
+++ puzzle_game/new_main.py
@@ -7,6 +7,8 @@ pygame.init()
 
 WIDTH = 880
 HEIGHT = 600
+MODE = 4
+
 
 pygame.display.set_caption("拼图")
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
@@ -14,6 +16,15 @@ clock = pygame.time.Clock()
 fps = 10
 
 image = pygame.image.load("elephant.jpg").convert()
+image_rect = image.get_rect()
+blocks = []
+for i in range(MODE):
+    for j in range(MODE):
+        block_width = WIDTH // MODE
+        block_height = HEIGHT // MODE
+        sub_rect = pygame.rect.Rect(j * block_width, i * block_height, block_width, block_height)
+        block_surf = image.subsurface(sub_rect)
+        blocks.append((block_surf, sub_rect))
 
 
 while True:
@@ -22,6 +33,10 @@ while True:
             pygame.quit()
             sys.exit(-1)
 
-    screen.blit(image, (0, 0))
+    # screen.blit(image, (0, 0))
+    for block_surf, block_rect in blocks:
+        screen.blit(block_surf, block_rect)
+        pygame.draw.rect(screen, "blue", block_rect, 1)
+
     pygame.display.flip()
     clock.tick(fps)
